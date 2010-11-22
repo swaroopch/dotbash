9da2bff0d57b65f5f377a83a665c31855824448d
Swaroop C H <swaroop@swaroopch.com>
2010-11-22 09:08:50 +0530

    Shortcut to read any Python egg's code

---
+++ plugins/python.plugin.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function egg_edit
+{
+    module="$1"
+    if [[ "$module" == "" ]]
+    then
+        echo "Usage: egg_edit <module name>"
+        return 1
+    fi
+
+    module_file=$(python -c "import $module; print $module.__file__")
+    module_directory=$(dirname $module_file)
+    vim -c ":NERDTree $module_directory"
+    # OR:
+    #   vim -c "$module_directory"
+    # for those who don't use NERDTree plugin
+}
