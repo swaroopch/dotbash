81df9ff7fc7e1242e1ceda181b7bed2b6bfe990a
Swaroop C H <swaroop@swaroopch.com>
2010-12-20 23:55:18 +0530

    Completing host nicknames only, not real names

--- completion/ssh.completion.bash
+++ completion/ssh.completion.bash
@@ -2,5 +2,5 @@
 
 if [[ -f "$HOME/.ssh/config" ]]
 then
-    complete -W "$(echo `cat $HOME/.ssh/config | grep -iE '^(Host|HostName)' | awk '{ print $2 }'`)" ssh
+    complete -W "$(echo `cat $HOME/.ssh/config | grep -iE '^Host' | awk '{ print $2 }'`)" ssh
 fi
