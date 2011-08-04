4ff03eba7eacd40e65ba25b13e742fdd35a4135f
Swaroop C H <swaroop@swaroopch.com>
2011-08-04 20:16:40 +0530

    Adding back egg_edit function

--- plugins/available/python.plugin.bash
+++ plugins/available/python.plugin.bash
@@ -7,3 +7,24 @@ else
   alias http='python -m SimpleHTTPServer'
 fi
 
+function egg_edit
+{
+    module="$1"
+    if [[ "$module" == "" ]]
+    then
+        echo "Usage: egg_edit <module name>"
+        return 1
+    fi
+
+    module_file=$(python -c "import $module; print $module.__file__")
+    if [[ "$module_file" == "" ]]
+    then
+        return 1
+    fi
+
+    module_directory=$(dirname $module_file)
+    vim -c ":NERDTree $module_directory"
+    # OR:
+    #   vim -c "$module_directory"
+    # for those who don't use NERDTree plugin
+}
