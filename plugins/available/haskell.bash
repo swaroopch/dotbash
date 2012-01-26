3a0fc0c8debb1cdd27f8c41d7dec9ffab20ff152
Swaroop C H <swaroop@swaroopch.com>
2012-01-26 11:33:19 +0530

    Adding Haskell PATH
    As per http://hackage.haskell.org/platform/mac.html installer

--- plugins/available/haskell.bash
+++ plugins/available/haskell.bash
@@ -1,2 +1,3 @@
 
-[[ -s $HOME/.cabal/bin ]] && export PATH=$HOME/.cabal/bin:$PATH
+[[ -s "$HOME/.cabal/bin" ]] && export PATH="$HOME/.cabal/bin:$PATH"
+[[ -s "$HOME/Library/Haskell/bin" ]] && export PATH="$HOME/Library/Haskell/bin:$PATH"
