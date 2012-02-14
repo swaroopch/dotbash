f638b4e33d86e7a6b2053574c9f394e89b222ce1
Swaroop C H <swaroop@swaroopch.com>
2012-02-14 11:30:20 -0800

    Simplifying PATH manipulation

--- template/my_bash_profile.bash
+++ template/my_bash_profile.bash
@@ -21,6 +21,18 @@ else
     exit 1
 fi
 
+## Helpers
+
+function prepend_path() {
+    dir=$1
+    shift
+
+    escaped_dir=${dir//\//\\\/}
+
+    # Two slashes means global replacement
+    export PATH="$dir:${PATH//$escaped_dir:/}"
+}
+
 ## Vim
 
 # Set my editor and git editor
@@ -37,33 +49,6 @@ then
 else
     export EDITOR='vim'
 fi
-
-## Python
-export VIRTUALENV_USE_DISTRIBUTE=1
-## http://jacobian.org/writing/when-pypi-goes-down/
-export PIP_USE_MIRRORS=1
-## This is needed when I have already set a 'venv' and I start tmux -
-## The prompt shows the venv name but the PATHs are not actually set
-## and the default 'python' is the system-wide default python.
-if [[ "$VIRTUAL_ENV" != "" ]]
-then
-    source "$VIRTUAL_ENV/bin/activate"
-fi
-
-## Ruby
-### Use RBENV if present
-if [[ -s $HOME/.rbenv ]]
-then
-    export PATH="$HOME/.rbenv/bin:$PATH"
-    eval "$(rbenv init -)"
-else
-    if [[ -d "/var/lib/gems/1.8/bin" ]]
-    then
-        export PATH="/var/lib/gems/1.8/bin:$PATH"
-    fi
-fi
-### Else use RVM
-#[[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm
 
 ## Bash
 
@@ -83,23 +68,47 @@ fi
 # Homebrew
 if [[ "$OS" == "mac" ]]
 then
-    # brew doctor
-    # http://mxcl.github.com/homebrew/
-    export PATH="${PATH/\/usr\/local\/bin:/}"  # Remove directory from PATH
-    export PATH="${PATH/\/usr\/local\/sbin:/}" # Remove directory from PATH
-    # Add to the head of the PATH
-    export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
+    # $(brew doctor)
+    prepend_path "/usr/local/sbin"
+    prepend_path "/usr/local/bin"
 
     # Ruby bin (because `brew install ruby` installs latest 1.9.3-p0 (as of this writing) and Mac OS X Lion has Ruby 1.8.7)
     if [[ "$(brew list | grep '^ruby$')" != "" ]]
     then
-        export PATH="$(brew --prefix ruby)/bin:$PATH"
+        prepend_path "$(brew --prefix ruby)/bin"
     fi
 fi
 
+## Python
+export VIRTUALENV_USE_DISTRIBUTE=1
+## http://jacobian.org/writing/when-pypi-goes-down/
+export PIP_USE_MIRRORS=1
+## This is needed when I have already set a 'venv' and I start tmux -
+## The prompt shows the venv name but the PATHs are not actually set
+## and the default 'python' is the system-wide default python.
+if [[ "$VIRTUAL_ENV" != "" ]]
+then
+    source "$VIRTUAL_ENV/bin/activate"
+fi
+
+## Ruby
+### Use RBENV if present
+if [[ -s "$HOME/.rbenv" ]]
+then
+    prepend_path "$HOME/.rbenv/bin"
+    eval "$(rbenv init -)"
+else
+    if [[ -d "/var/lib/gems/1.8/bin" ]]
+    then
+        prepend_path "/var/lib/gems/1.8/bin"
+    fi
+fi
+## Else use RVM
+#[[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm
+
 ## System
 
-[[ -d "$HOME/bin" ]] && export PATH="$HOME/bin:$PATH"
+[[ -d "$HOME/bin" ]] && prepend_path "$HOME/bin"
 
 ## Cleanup
 
